c3615859d8565b8c5984e99641f17a049b78883d
Dmytro Kopaihora <47076154+Dimonjik777@users.noreply.github.com>
2024-09-20 15:33:07 +0300

    Finalizing the code

--- src/js/index.js
+++ src/js/index.js
@@ -6,8 +6,8 @@ let taskInput = document.getElementById("taskInput");
 
 document.addEventListener("keydown", event => {
 
-  if(event.key == "Enter" && taskInput.value != ""){
-    
+  if (event.key == "Enter" && taskInput.value != "") {
+
     createTask(taskInput.value);
     taskInput.value = "";
   }
@@ -16,39 +16,38 @@ document.addEventListener("keydown", event => {
 document.addEventListener("click", event => {
 
   // Check click to complete button
-  if(event.target.classList.contains("no_complete")){
-    
+  if (event.target.classList.contains("no_complete")) {
+
     // Get id from button
     let idTask = event.target.getAttribute("id");
 
     // Find parent on this button
     let parent = event.target.parentElement;
     checkedTask(idTask);
-    
+
 
     //Check button and text and stylizing
-    if(tasks[idTask][1]){
+    if (tasks[idTask].isComplete) {
       event.target.classList.add("complete");
       parent.querySelector("p").classList.add("complete_task");
     }
-    else{
+    else {
       event.target.classList.remove("complete");
       parent.querySelector("p").classList.remove("complete_task");
     }
   }
 
   // Check click to delete button
-  if(event.target.classList.contains("delete")){
-    
-     // Get id from button
+  if (event.target.classList.contains("delete")) {
+
+    // Get id from button
     let idTask = event.target.getAttribute("id");
     // Find parent on this button
     let parent = event.target.parentElement;
 
     parent.remove();
     deleteTask(idTask);
-    
-    console.log(parent);
+
   }
 
 });
@@ -57,8 +56,9 @@ function render() {
 
   tasksContainer.innerHTML = "";
 
-  for(let i = 0; i < tasks.length; i++){
-    
+  for (let i = 0; i < tasks.length; i++) {
+
+
     // Create components for HTML
     let completeButton = document.createElement("button");
     let textTask = document.createElement("p");
@@ -77,7 +77,7 @@ function render() {
     taskHTML.setAttribute("task-id", i);
 
     // Add content
-    textTask.textContent = tasks[i][0];
+    textTask.textContent = tasks[i].task;
 
     // Add in parent container
     taskHTML.append(completeButton);
@@ -88,29 +88,17 @@ function render() {
 }
 
 function createTask(task) {
-  tasks.unshift([task, false]);
+  tasks.unshift({ task: task, isComplete: false });
   render();
 }
 
 function deleteTask(id) {
 
-  // Delete first element
-  if(id == 0)
-    tasks.shift();
-
-  // Delete other element
-  else
-    tasks.splice(id, id);
-
+  tasks.splice(id, 1);
   render();
 }
 
 function checkedTask(id) {
 
-  if (!tasks[id][1]) {
-    tasks[id][1] = true;
-  }
-  else {
-    tasks[id][1] = false;
-  }
+  tasks[id].isComplete = !tasks[id].isComplete;
 }
