7b18803ad87ce46f6a9256085498eac61f5ce229
Dmytro Kopaihora <47076154+Dimonjik777@users.noreply.github.com>
2024-09-23 16:48:11 +0300

    Add features
    Added a filter for sorting tasks by execution and storage in localStorage

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,24 @@
 // Declare variables
 
-let tasks = [];
+// Take tasks from local storage or empty array
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+// Take DOM elements
 let tasksContainer = document.querySelector(".tasks");
 let taskInput = document.getElementById("taskInput");
+let totalTasks = document.querySelector(".tasks__total");
+let tasksComplete = document.querySelector(".tasks__complete");
+let tasksNoComplete = document.querySelector(".tasks__nocomplete");
+
+// State variables
+let isCompleteTasks = false;
+let noCompleteTasks = false;
+
+// Counters
+let countCompleteTasks = tasks.filter(task => task.isComplete).length;
+let countNoCompleteTasks = 0;
+
+render();
 
 document.addEventListener("keydown", event => {
 
@@ -42,63 +58,158 @@ document.addEventListener("click", event => {
 
     // Get id from button
     let idTask = event.target.getAttribute("id");
-    // Find parent on this button
-    let parent = event.target.parentElement;
-
-    parent.remove();
+    console.log(idTask);
     deleteTask(idTask);
 
+  }
+
+  // Check click to tasks complete
+  if (event.target.classList.contains("tasks__complete")) {
+    showCompleteTasks();
+  }
+
+  // Check click to tasks no complete
+  if(event.target.classList.contains("tasks__nocomplete")){
+    showNoCompleteTasks();
   }
 
 });
 
 function render() {
 
+  // Total no complete tasks
+  countNoCompleteTasks = tasks.length - countCompleteTasks;
+
   tasksContainer.innerHTML = "";
 
-  for (let i = 0; i < tasks.length; i++) {
+  // Render only complete tasks
+  if (isCompleteTasks) {
 
+    for (let i = 0; i < tasks.length; i++) {
 
-    // Create components for HTML
-    let completeButton = document.createElement("button");
-    let textTask = document.createElement("p");
-    let deleteButton = document.createElement("button");
-    let taskHTML = document.createElement("div");
+      if (tasks[i].isComplete) {
 
-    // Add classes
-    textTask.classList.add("text__task");
-    completeButton.classList.add("no_complete");
-    deleteButton.classList.add("delete");
-    taskHTML.classList.add("task");
+        renderHTML(i);
+      }
 
-    // Add attributes
-    completeButton.setAttribute("id", i);
-    deleteButton.setAttribute("id", i);
-    taskHTML.setAttribute("task-id", i);
-
-    // Add content
-    textTask.textContent = tasks[i].task;
-
-    // Add in parent container
-    taskHTML.append(completeButton);
-    taskHTML.append(textTask);
-    taskHTML.append(deleteButton);
-    tasksContainer.append(taskHTML);
+    }
   }
+
+  // Render no complete tasks
+  else if(noCompleteTasks){
+    
+    for (let i = 0; i < tasks.length; i++) {
+
+      if(!tasks[i].isComplete){
+
+        renderHTML(i);
+
+      }
+
+      
+    }
+
+  }
+
+  // Render all tasks
+  else{
+
+    for (let i = 0; i < tasks.length; i++) {
+
+      renderHTML(i);
+    }
+  }
+
+  //Render count total tasks, complete tasks no complete
+
+  totalTasks.textContent = `Total tasks: ${tasks.length}`;
+  tasksComplete.textContent = `Tasks complete: ${countCompleteTasks}`;
+  tasksNoComplete.textContent = `Tasks: ${countNoCompleteTasks}`;
+
 }
+
+function renderHTML(i){
+
+  // Create components for HTML
+  let completeButton = document.createElement("button");
+  let textTask = document.createElement("p");
+  let deleteButton = document.createElement("button");
+  let taskHTML = document.createElement("div");
+
+  // Add classes
+  textTask.classList.add("text__task");
+  completeButton.classList.add("no_complete");
+  deleteButton.classList.add("delete");
+  taskHTML.classList.add("task");
+
+  // Render complete task
+  if (tasks[i].isComplete) {
+    completeButton.classList.add("complete");
+    textTask.classList.add("complete_task");
+  }
+
+  // Add attributes
+  completeButton.setAttribute("id", i);
+  deleteButton.setAttribute("id", i);
+  taskHTML.setAttribute("task-id", i);
+
+  // Add content
+  textTask.textContent = tasks[i].task;
+
+  // Add in parent container
+  taskHTML.append(completeButton);
+  taskHTML.append(textTask);
+  taskHTML.append(deleteButton);
+  tasksContainer.append(taskHTML);
+
+}
+
 
 function createTask(task) {
   tasks.unshift({ task: task, isComplete: false });
+  
+  // Save task in local storage
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  console.log(JSON.parse(localStorage.getItem("tasks")));
   render();
 }
 
 function deleteTask(id) {
 
+  // Remove task in local storage
   tasks.splice(id, 1);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
   render();
 }
 
 function checkedTask(id) {
 
   tasks[id].isComplete = !tasks[id].isComplete;
+
+  if(tasks[id].isComplete){
+    countCompleteTasks++;
+  }
+  else{
+    countCompleteTasks--;
+  }
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  render();
+}
+
+function showCompleteTasks() {
+  if(noCompleteTasks){
+    noCompleteTasks = !noCompleteTasks;
+  }
+  isCompleteTasks = !isCompleteTasks;
+  render();
+}
+
+function showNoCompleteTasks() {
+  
+  if(isCompleteTasks){
+    isCompleteTasks = !isCompleteTasks;
+  }
+  noCompleteTasks = !noCompleteTasks;
+  render();
 }
